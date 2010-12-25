d9bd146068d1db66956287a78dda3d0b16acdb73
עידו פרלמוטר (Ido Perlmuter) <ido@ido50.net>
2010-12-25 18:40:30 +0000

    Import of IDOPEREL/MongoDBx-Class-0.1 from CPAN.
    gitpan-cpan-distribution: MongoDBx-Class
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         IDOPEREL/MongoDBx-Class-0.1.tar.gz
    gitpan-cpan-author:       IDOPEREL
    gitpan-cpan-maturity:     released

---
+++ lib/MongoDBx/Class/Connection.pm
@@ -0,0 +1,302 @@
+package MongoDBx::Class::Connection;
+BEGIN {
+  $MongoDBx::Class::Connection::VERSION = '0.1';
+}
+
+# ABSTARCT: A connection to a MongoDB server
+
+use Moose;
+use namespace::autoclean;
+use Scalar::Util qw/blessed/;
+
+extends 'MongoDB::Connection';
+
+=head1 NAME
+
+MongoDBx::Class::Connection - A connection to a MongoDB server
+
+=head1 VERSION
+
+version 0.1
+
+=head1 EXTENDS
+
+L<MongoDB::Connection>
+
+=head1 SYNOPSIS
+
+	# connect to a MongoDB server
+	$mongodbx->connect(host => '10.10.10.10', port => 27017);
+
+	# the connection object is automatically saved to the 'conn'
+	# attribute of the L<MongoDBx::Class> object (C<$mongodbx> above)
+
+	$mongodbx->conn->get_database('people');
+
+=head1 DESCRIPTION
+
+MongoDBx::Class::Connection extends L<MongoDB::Connection>. This class
+provides the document expansion and collapsing methods that are used
+internally by other MongoDBx::Class classes.
+
+Note that a L<MongoDBx::Class> object can only have one connection at a
+time. Connection is only made via the C<connect()> method in MongoDBx::Class.
+
+=head1 ATTRIBUTES
+
+Aside for attributes provided by L<MongoDB::Connection>, the following
+special attributes are added:
+
+=head2 namespace
+
+A string representing the namespace of document classes to load (e.g.
+MyApp::Schema). This is a required attribute.
+
+=head2 doc_classes
+
+A hash-ref of document classes loaded. This is a required attribute.
+
+=head2 safe
+
+A boolean value indicating whether to use safe operations (e.g. inserts
+and updates) by default - without the need to pass C<<{ safe => 1 }>> to
+relevant methods - or not. False by default.
+
+=cut
+
+has 'namespace' => (is => 'ro', isa => 'Str', required => 1);
+
+has 'doc_classes' => (is => 'ro', isa => 'HashRef', required => 1);
+
+has 'safe' => (is => 'rw', isa => 'Bool', default => 0);
+
+=head1 OBJECT METHODS
+
+Aside from the methods provided by L<MongoDB::Connection>, the following
+methods and modifications are added:
+
+=head2 get_database( $name )
+
+Returns a L<MongoDBx::Class::Database> object representing the MongoDB
+database named C<$name>.
+
+=cut
+
+override 'get_database' => sub {
+	MongoDBx::Class::Database->new(_connection => shift, name => shift);
+};
+
+=head2 safe( $boolean )
+
+Overrides the current value of the safe attribute with a new boolean value.
+
+=head2 expand( $coll_ns, \%doc )
+
+Returns the full name (a.k.a namespace) of a collection (that is the database
+name, followed by a dot, and the collection name), and a document hash-ref,
+and attempts to expand it according to the _class attribute that should
+exists in the document. If it doesn't exist, the document is returned as
+is.
+
+This is mostly used internally and you don't have to worry about expansion,
+it's done automatically.
+
+=cut
+
+sub expand {
+	my ($self, $coll_ns, $doc) = @_;
+
+	return unless $coll_ns && $doc && ref $doc eq 'HASH';
+
+	my ($db_name, $coll_name) = ($coll_ns =~ m/^([^.]+)\.(.+)$/);
+
+	my $coll = $self->get_database($db_name)->get_collection($coll_name);
+
+	return $doc unless exists $doc->{_class};
+
+	my $dc_name = $doc->{_class};
+	my $ns = $self->namespace;
+	$dc_name =~ s/^${ns}:://;
+
+	my $dc = $self->doc_classes->{$dc_name};
+
+	return $doc unless $dc;
+
+	my %attrs = (
+		_collection => $coll,
+		_class => $doc->{_class},
+	);
+
+	foreach ($dc->meta->get_all_attributes) {
+		# is this a MongoDBx::Class::Reference?
+		if ($_->{isa} eq 'MongoDBx::Class::CoercedReference') {
+			my $name = $_->name;
+			$name =~ s!^_!!;
+			
+			next unless exists $doc->{$name} &&
+				    defined $doc->{$name} && 
+				    ref $doc->{$name} eq 'HASH' &&
+				    exists $doc->{$name}->{'$ref'} &&
+				    exists $doc->{$name}->{'$id'};
+
+			$attrs{$_->name} = MongoDBx::Class::Reference->new(
+				_collection => $coll,
+				_class => 'MongoDBx::Class::Reference',
+				ref_coll => $doc->{$name}->{'$ref'},
+				ref_id => $doc->{$name}->{'$id'},
+			);
+		} elsif ($_->{isa} eq 'ArrayOfMongoDBx::Class::CoercedReference') {
+			my $name = $_->name;
+			$name =~ s!^_!!;
+
+			next unless exists $doc->{$name} &&
+				    defined $doc->{$name} && 
+				    ref $doc->{$name} eq 'ARRAY';
+
+			foreach my $ref (@{$doc->{$name}}) {
+				push(@{$attrs{$_->name}}, MongoDBx::Class::Reference->new(
+					_collection => $coll,
+					_class => 'MongoDBx::Class::Reference',
+					ref_coll => $ref->{'$ref'},
+					ref_id => $ref->{'$id'},
+				));
+			}				
+		} elsif ($_->documentation && $_->documentation eq 'MongoDBx::Class::EmbeddedDocument') {
+			my $edc_name = $_->{isa};
+			$edc_name =~ s/^${ns}:://;
+			if ($_->{isa} =~ m/^ArrayRef/) {
+				my $name = $_->name;
+				$name =~ s!^_!!;
+				
+				$edc_name =~ s/^ArrayRef\[//;
+				$edc_name =~ s/\]$//;
+
+				next unless exists $doc->{$name} &&
+					    defined $doc->{$name} && 
+					    ref $doc->{$name} eq 'ARRAY';
+
+				$attrs{$_->name} = [];
+
+				foreach my $a (@{$doc->{$name}}) {
+					$a->{_class} = $edc_name;
+					push(@{$attrs{$_->name}}, $self->expand($coll_ns, $a));
+				}
+			} else {
+				next unless exists $doc->{$_->name} && defined $doc->{$_->name};
+				$doc->{$_->name}->{_class} = $edc_name;
+				$attrs{$_->name} = $self->expand($coll_ns, $doc->{$_->name});
+			}
+		} else {
+			next unless exists $doc->{$_->name} && defined $doc->{$_->name};
+			$attrs{$_->name} = $doc->{$_->name};
+		}
+	}
+
+	return $dc->new(%attrs);
+}
+
+=head2 collapse( $val )
+
+Receives a value for a document attribute and returns it after collapsion
+so that it can be safely inserted to the database. For example, you can't
+save an embedded document directly to the database, you need to convert
+it to a hash-ref first.
+
+This method is mostly used internally and you don't have to worry about
+collapsing, it's done automatically.
+
+=cut
+
+sub collapse {
+	my ($self, $val) = @_;
+
+	if (ref $val eq 'ARRAY') {
+		my @arr;
+		foreach (@$val) {
+			if (blessed $_ && $_->isa('MongoDBx::Class::Reference')) {
+				push(@arr, { '$ref' => $_->ref_coll, '$id' => $_->ref_id });
+			} elsif (blessed $_ && $_->does('MongoDBx::Class::Document')) {
+				push(@arr, { '$ref' => $_->_collection->name, '$id' => $_->_id });
+			} elsif (blessed $_ && $_->does('MongoDBx::Class::EmbeddedDocument')) {
+				my $hash = {};
+				foreach my $ha (keys %$_) {
+					next if $ha eq '_collection';
+					$hash->{$ha} = $_->{$ha};
+				}
+				push(@arr, $hash);
+			} else {
+				push(@arr, $_);
+			}
+		}
+		return \@arr;
+	} elsif (blessed $val && $val->isa('MongoDBx::Class::Reference')) {
+		return { '$ref' => $val->ref_coll, '$id' => $val->ref_id };
+	} elsif (blessed $val && $val->does('MongoDBx::Class::Document')) {
+		return { '$ref' => $val->_collection->name, '$id' => $val->_id };
+	} elsif (blessed $val && $val->does('MongoDBx::Class::EmbeddedDocument')) {
+		my $hash = {};
+		foreach my $ha (keys %$val) {
+			next if $ha eq '_collection';
+			$hash->{$ha} = $val->{$ha};
+		}
+		return $hash;
+	}
+
+	return $val;
+}
+
+=head1 AUTHOR
+
+Ido Perlmuter, C<< <ido at ido50.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mongodbx-class at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MongoDBx-Class>. I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+	perldoc MongoDBx::Class::Connection
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MongoDBx::Class>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MongoDBx::Class>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MongoDBx::Class>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MongoDBx::Class/>
+
+=back
+
+=head1 SEE ALSO
+
+L<MongoDBx::Class>, L<MongoDB::Connection>.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Ido Perlmuter.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
---
+++ lib/MongoDBx/Class/Document.pm
@@ -0,0 +1,274 @@
+package MongoDBx::Class::Document;
+BEGIN {
+  $MongoDBx::Class::Document::VERSION = '0.1';
+}
+
+# ABSTRACT: A MongoDBx::Class document role
+
+use Moose::Role;
+use namespace::autoclean;
+
+=head1 NAME
+
+MongoDBx::Class::Document - A MongoDBx::Class document role
+
+=head1 VERSION
+
+version 0.1
+
+=head1 SYNOPSIS
+
+	# create a document class
+	package MyApp::Schema::Novel;
+
+	use MongoDBx::Class::Moose; # use this instead of Moose;
+	use namespace::autoclean;
+
+	has 'title' => (is => 'ro', isa => 'Str', required => 1, writer => 'set_title');
+
+	holds_one 'author' => (is => 'ro', isa => 'PersonName', required => 1, writer => 'set_author');
+
+	has 'year' => (is => 'ro', isa => 'Int', predicate => 'has_year', writer => 'set_year');
+	
+	holds_many 'tags' => (is => 'ro', isa => 'Tag', predicate => 'has_tags');
+
+	joins_one 'synopsis' => (is => 'ro', isa => 'Synopsis', coll => 'synopsis', ref => 'novel');
+
+	has_many 'related_novels' => (is => 'ro', isa => 'Novel', predicate => 'has_related_novels', writer => 'set_related_novels', clearer => 'clear_related_novels');
+
+	joins_many 'reviews' => (is => 'ro', isa => 'Review', coll => 'reviews', ref => 'novel');
+
+	sub print_related_novels {
+		my $self = shift;
+
+		foreach my $other_novel ($self->related_novels) {
+			print $other_novel->title, ', ',
+			      $other_novel->year, ', ',
+			      $other_novel->author->name, "\n";
+		}
+	}
+
+	around 'reviews' => sub {
+		my ($orig, $self) = (shift, shift);
+
+		my $cursor = $self->$orig;
+		
+		return $cursor->sort([ year => -1, title => 1, 'author.last_name' => 1 ]);
+	};
+
+	__PACKAGE__->meta->make_immutable;
+
+=head1 DESCRIPTION
+
+MongoDBx::Class::Document is a L<Moose role|Moose::Role> meant to be consumed
+by document classes. It provides expanded MongoDB documents with some
+common attributes, and needed methods for easy updating and deleting of
+documents.
+
+=head1 ATTRIBUTES
+
+The following attributes are provided:
+
+=head2 _id
+
+The document's internal ID, represented as a L<MongoDB::OID> object. This
+is a required attribute.
+
+=head2 _collection
+
+The L<MongoDBx::Class::Collection> object representing the MongoDB collection
+in which the document is stored. This is a required attribute.
+
+=head2 _class
+
+A string. The name of the document class of this document. This is a required
+attribute.
+
+=cut
+
+has '_id' => (is => 'ro', isa => 'MongoDB::OID', required => 1);
+
+has '_collection' => (is => 'ro', isa => 'MongoDBx::Class::Collection', required => 1);
+
+has '_class' => (is => 'ro', isa => 'Str', required => 1);
+
+=head1 OBJECT METHODS
+
+The following object methods are provided:
+
+=head2 id()
+
+=head2 oid()
+
+Both methods are equivalent. They are convenience methods that return
+the documents internal MongoDB OID in string format.
+
+=cut
+
+sub id {
+	shift->_id->to_string;
+}
+
+sub oid {
+	shift->id;
+}
+
+=head2 update( [ \%object ], [ \%options ] )
+
+Saves a new version of the document to the database. The behavior of this
+method is dependant on the existance of an object hash-ref:
+
+If an object hash-ref is provided, all of its key-value pairs are collapsed,
+and a C<$set> update is performed on them. For example:
+
+	$doc->update({ author => 'Sir Arthur Conan Doyle', year => 1895 })
+
+Will effectively result in something like this being performed:
+
+	$coll->update({ _id => $doc->_id }, { '$set' => { author => 'Sir Arthur Conan Doyle', year => 1895 } }, $options)
+
+If an object hash-ref isn't provided, the entire document object is collapsed
+and an aggresive update is performed (i.e. an entirely new version of the
+document, representing the current state of the document's attributes, is
+saved to the database. For example:
+
+	my $doc = find_one($id);
+
+	$doc->set_author('Sir Arthur Conan Doyle');
+	$doc->set_year(1895);
+	$doc->update;
+
+Will effectively result in something like this being performed:
+
+	$coll->update({ _id => $doc->_id }, $collapsed_doc, $options)
+
+You can pass an options hash-ref just like with the C<update()> method
+of L<MongoDBx::Class::Collection>.
+
+=cut
+
+sub update {
+	my $self = shift;
+
+	if (scalar @_ && ref $_[0] eq 'HASH') {
+		foreach (keys %{$_[0]}) {
+			$_[0]->{$_} = $self->_connection->collapse($_[0]->{$_});
+		}
+		$self->_collection->update({ _id => $self->_id }, { '$set' => $_[0] }, $_[1]);
+	} else {
+		my $new_doc;
+		foreach (ref($self)->meta->get_all_attributes) {
+			my $name = $_->name;
+			next if $name eq '_collection' || $name eq '_class';
+
+			my $newval = $self->_connection->collapse($self->$name);
+
+			$name =~ s/^_// if ($_->{isa} eq 'MongoDBx::Class::CoercedReference' ||
+					    $_->{isa} eq 'ArrayOfMongoDBx::Class::CoercedReference' ||
+					    ($_->documentation && $_->documentation eq 'MongoDBx::Class::EmbeddedDocument')
+					   );
+
+			$new_doc->{$name} = $newval;
+		}
+		$new_doc->{_class} = $self->_class;
+		$self->_collection->update({ _id => $self->_id }, $new_doc, $_[1]);
+	}
+}
+
+=head2 delete()
+
+=head2 remove()
+
+Both methods are equivalent. They are shortcut methods for invoking the
+collections C<remove()> method on this document only. So, umm, they remove
+the document. But note that this operation does not cascade, so documents
+which are considered dependant on this document (such as those that reference
+it with C<belongs_to>) will not be removed too.
+
+=cut
+
+sub delete {
+	my $self = shift;
+
+	$self->_collection->remove({ _id => $self->_id });
+}
+
+sub remove {
+	shift->delete;
+}
+
+=head2 _database()
+
+Convenience method, shortcut for C<<$doc->_collection->_database>>.
+
+=cut
+
+sub _database {
+	shift->_collection->_database;
+}
+
+=head2 _connection()
+
+Convenience method, shortcut for C<<$doc->_connection->_database>>.
+
+=cut
+
+sub _connection {
+	shift->_database->_connection;
+}
+
+=head1 AUTHOR
+
+Ido Perlmuter, C<< <ido at ido50.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mongodbx-class at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MongoDBx-Class>. I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+	perldoc MongoDBx::Class::Document
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MongoDBx::Class>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MongoDBx::Class>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MongoDBx::Class>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MongoDBx::Class/>
+
+=back
+
+=head1 SEE ALSO
+
+L<MongoDBx::Class::Moose>, L<MongoDBx::Class::EmbeddedDocument>.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Ido Perlmuter.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
